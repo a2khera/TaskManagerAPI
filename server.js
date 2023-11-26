3be7e6c975160f97cab8c272fd6a059f5a466221
Abhinav Khera <abhinavkhera123@gmail.com>
2023-11-26 14:50:42 -0500

    Add error handling to server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 app.use(express.json()); // for parsing application/json
 app.use('/tasks', taskRoutes); // Use task routes
 
+// For Error Handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!'); 
+});
+
 // Server listens on environment-defined port or 3000 as default
 const PORT = process.env.PORT || 3000; 
 
